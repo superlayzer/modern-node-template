7665ed3757d7d7c4fdc23de8a39b0af6192d1f5d
Junayed <hello@layzer.sh>
2025-08-14 11:01:22 +0100

    feat: add ESLint and Prettier with default configs

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
